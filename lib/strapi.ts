f16b3adc3af270ed95f0b8fa1e9869921a0cd984
buraq <buraq@192.168.10.5>
2026-06-05 13:17:53 +0800

    fix: API_URL3

--- lib/strapi.ts
+++ lib/strapi.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { Product, ProductImage } from '@/types/product';
 
-const STRAPI_URL = process.env.NEXT_PUBLIC_STRAPI_URL;
+const STRAPI_URL = process.env.NEXT_PUBLIC_STRAPI_URL ?? 'https://zh-backend-production.up.railway.app/api';
+
 const STRAPI_BASE_URL = STRAPI_URL.replace('/api', '');
 
 const api = axios.create({ baseURL: STRAPI_URL });
